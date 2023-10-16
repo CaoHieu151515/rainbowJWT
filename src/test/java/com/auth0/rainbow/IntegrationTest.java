cd1f7400fe5557ffb31974d01414a9939c647fc3
CaoHieu151515 <caotronghieu151515@gmail.com>
2023-10-16 09:12:01 +0700

    Initial version of rainbow generated by generator-jhipster@7.9.4

---
+++ src/test/java/com/auth0/rainbow/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.auth0.rainbow;
+
+import com.auth0.rainbow.RainbowApp;
+import com.auth0.rainbow.config.AsyncSyncConfiguration;
+import com.auth0.rainbow.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { RainbowApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
