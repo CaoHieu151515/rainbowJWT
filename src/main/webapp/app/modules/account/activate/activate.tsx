ce7a3f36416492dc569a1a0cd4854192ee1844dd
CaoHieu151515 <caotronghieu151515@gmail.com>
2023-10-28 18:28:59 +0700

    adasd

--- src/main/webapp/app/modules/account/activate/activate.tsx
+++ src/main/webapp/app/modules/account/activate/activate.tsx
@@ -1,38 +1,53 @@
 import React, { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { Row, Col, Alert } from 'reactstrap';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { activateAction, reset } from './activate.reducer';
+
+const successAlert = (
+  <Alert color="success">
+    <strong>Your user account has been activated.</strong> Please
+    <Link to="/login" className="alert-link">
+      sign in
+    </Link>
+    .
+  </Alert>
+);
+
+const failureAlert = (
+  <Alert color="danger">
+    <strong>Your user could not be activated.</strong> Please use the registration form to sign up.
+  </Alert>
+);
 
 export const ActivatePage = () => {
   const dispatch = useAppDispatch();
 
   const [searchParams] = useSearchParams();
 
+  useEffect(() => {
+    const key = searchParams.get('key');
+
+    dispatch(activateAction(key));
+    return () => {
+      dispatch(reset());
+    };
+  }, []);
+
   const { activationSuccess, activationFailure } = useAppSelector(state => state.activate);
 
   return (
-    <div style={{ visibility: 'hidden', display: 'none' }}>
+    <div>
       <Row className="justify-content-center">
         <Col md="8">
           <h1>Activation</h1>
-          {activationSuccess ? (
-            <Alert color="success">
-              <strong>Your user account has been activated.</strong>
-            </Alert>
-          ) : undefined}
-          {activationFailure ? (
-            <Alert color="danger">
-              <strong>Your user could not be activated.</strong>
-            </Alert>
-          ) : undefined}
+          {activationSuccess ? successAlert : undefined}
+          {activationFailure ? failureAlert : undefined}
         </Col>
       </Row>
     </div>
   );
 };
-useEffect(() => {
-  window.location.href = 'https://vnrainbow.vercel.app'; // Thay thế bằng URL của trang bạn muốn chuyển hướng đến
-}, []);
+
 export default ActivatePage;
